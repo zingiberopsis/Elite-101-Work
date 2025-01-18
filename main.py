17c22da366de226c01c339efcd47edf3e6624aaa
zingiberopsis <scottm112008@gmail.com>
2025-01-18 00:04:45 +0000

    major editing to the code

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
-# By Scott
+print('Welcome to the shower question chatbot!')
+name = input('First, what is your name? ')
+age = input(f'Hello {name}, how old are you? ')
 
-print('Welcome to the random question chatbot!')
-name = input('What is your name? ')
-# name to be added into age line (simplicity!)
-age = input(f'Hello, {name}! How old are you? ')
-# no clue why we need it, but might as well add it
 
 def menu():
-    print('[1] Can bees fly?')
-    # yes
-    print('[2] Best loyalist space marine chapter (faction) in Warhammer 40k?')
-    # salamanders!
-    print('[3] Who let the dogs out?')
-    # baha men
-    print('[4] Exit.')
-    #end script
+  print('[1] What is the best legion in Warhammer 40k?') #as
+  print('[2] Why can bees fly?')
+  print('[3] What is the answer to life, the universe, and everything?')
+  print('[4] Exit.')
 
 menu()
-option = int(input('Enter your option: '))
+option = int(input('Enter the option you would like assistance with.'))
 
-while option != 4:
-    if option == 1:
-        print('Somehow, they can.')
-    elif option == 2:
-        print('Salamanders!')
-    elif option == 3:
-        print('The baha men.')
-    elif option == 42:
-        print('Answer to life, the universe, and everything.')
-    elif option == 66:
-        print('This order has already been executed by Lord Palpatine.')
-    elif option == 1993:      
-        print('Welcome, to Jurrasic Park..!')  
-    else:
-        print('Choose another option.')
+while option != 0:
+  if option ==1:
+    print('Salamanders! They defend humans, and keep their memories of being a normal human.')
+  elif option ==2:
+    print('They have wings, and weight surprisingly little.')
+  elif option ==3:
+    print('42.')
+  else:
+    print('Invalid Option, choose a digit.')
 
-    print()
-    menu()
-    option = int(input('Enter your option: '))
-print('Thank you for using our program. Goodbye!')
-# end script
+  print()
+  menu()
+  option = int(input('Enter the option you would like assistance with.'))
+print('Thanks for using this program, goodbye!') 
